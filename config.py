9d01ee58fbdc6f9d1dbe14eaa354ee0f24d9c0df
La1zzy <150120351+La1zzy@users.noreply.github.com>
2024-12-23 04:29:08 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = (
-        "postgresql://avnadmin:AVNS_qSY-E_GGG1SESLMzcOv@pg-39cec8a9-istu-7a4b.k.aivencloud.com:25134/defaultdb?sslmode=require"  
-    )
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
